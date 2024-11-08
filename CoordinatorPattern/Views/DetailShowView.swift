e60dd1b3818e7789dd4b241d0d6434841b9981cf
Manu Salinas <manuels@bluetrailsoft.com>
2024-11-08 15:46:15 -0600

    Show Detail now is medium and large, New Close Button animation

--- CoordinatorPattern/Views/DetailShowView.swift
+++ CoordinatorPattern/Views/DetailShowView.swift
@@ -10,71 +10,82 @@ import SwiftUI
 struct DetailShowView: View {
     @EnvironmentObject private var appCoordinator: AppCoordinator
     @State private var isAnimated = false
+    @State private var isLargeDetent = false
 
     let show: Show
 
     var body: some View {
-        ScrollView {
+        GeometryReader { geometry in
+            ScrollView {
 
-            ZStack {
-                AsyncImage(url: URL(string: show.picUrl!)) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .opacity(isAnimated ? 1 : 0)
-                        .animation(.easeInOut(duration: 1), value: self.isAnimated)
-                        .onAppear {
-                            self.isAnimated = true
+                ZStack {
+                    AsyncImage(url: URL(string: show.picUrl!)) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .opacity(isAnimated ? 1 : 0)
+                            .animation(.easeInOut(duration: 1), value: self.isAnimated)
+                            .onAppear {
+                                self.isAnimated = true
+                            }
+
+                    } placeholder: {
+                        ProgressView()
+                    }
+
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Button {
+                                appCoordinator.dismissSheet()
+                            } label: {
+                                Image(systemName: "xmark.circle.fill")
+                                    .font(.largeTitle)
+                            }
+                            .frame(width: 40, height: 40)
+                            .tint(.white)
+                            .shadow(color: .black.opacity(0.3), radius: 10)
                         }
-                    
-                } placeholder: {
-                    ProgressView()
+                        .padding()
+
+                        Spacer()
+                    }
+                    .opacity(isLargeDetent ? 1 : 0)
+                    .animation(.easeInOut, value: isLargeDetent)
                 }
 
                 VStack {
                     HStack {
+                        Text(show.name)
+                            .font(.system(size: 50, weight: .semibold, design: .rounded))
+                            .padding(.leading, 10)
                         Spacer()
-                        Button {
-                            appCoordinator.dismissSheet()
-                        } label: {
-                            Image(systemName: "xmark.circle.fill")
-                                .font(.largeTitle)
-                        }
-                        .frame(width: 40, height: 40)
-                        .tint(.white)
-                        .shadow(color: .black.opacity(0.3), radius: 10)
                     }
-                    .padding()
 
-                    Spacer()
+                    Spacer(minLength: 15)
+
+                    Text(show.info)
+                        .font(.title2)
+                        .fontDesign(.rounded)
+                        .padding(.horizontal, 10)
+
+                    Spacer(minLength: 50)
                 }
                 .opacity(isAnimated ? 1 : 0)
-                .animation(.easeInOut.delay(1.5), value: isAnimated)
+                .animation(.easeInOut.delay(1), value: isAnimated)
+
             }
-
-            VStack {
-                HStack {
-                    Text(show.name)
-                        .font(.system(size: 50, weight: .semibold, design: .rounded))
-                        .padding(.leading, 10)
-                    Spacer()
-                }
-
-                Spacer(minLength: 15)
-
-                Text(show.info)
-                    .font(.title2)
-                    .fontDesign(.rounded)
-                    .padding(.horizontal, 10)
-
-                Spacer(minLength: 50)
+            .ignoresSafeArea()
+            .onChange(of: geometry.size.height) { oldValue, newValue in
+                print(newValue)
+                updateDetentStatus(with: newValue)
             }
-            .opacity(isAnimated ? 1 : 0)
-            .animation(.easeInOut.delay(1), value: isAnimated)
-
         }
-        .ignoresSafeArea()
     }
+
+    private func updateDetentStatus(with height: CGFloat) {
+           isLargeDetent = height > 600
+       }
 }
 
 #Preview {
